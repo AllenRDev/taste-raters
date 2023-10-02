2cf2145e413489ed2d6fff602ddbf8da498f7ee1
Allen Roberts <aroberts@elinkdesign.com>
2023-10-02 10:03:55 -0400

    Styling Changes for forgot password page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,7 +3,7 @@ export default function Guest({ children }) {
     return (
         <div className="min-h-screen bg-gradient-to-r from-slate-200 to-orange-200">
         <div className="flex flex-col justify-center sm:py-12">
-            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+            <div className="relative py-3 sm:max-w-xl sm:mx-auto sm:my-0 mt-40">
                 <div className="absolute inset-0 bg-gradient-to-r from-orange-200 to-orange-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
                 <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
                     {children}
--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -2,7 +2,7 @@ import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import { Head, useForm } from '@inertiajs/react';
+import { Head, Link ,useForm } from '@inertiajs/react';
 
 export default function ForgotPassword({ status }) {
     const { data, setData, post, processing, errors } = useForm({
@@ -18,8 +18,13 @@ export default function ForgotPassword({ status }) {
     return (
         <GuestLayout>
             <Head title="Forgot Password" />
+            <div className="mb-4 text-sm text-orange-200 w-3/4 mx-auto text-center">
+            <Link href={route('login')} className="text-orange-200/80 hover:text-orange-100 text-sm transition-colors duration-300">
+                    <span className='material-symbols-outlined'>arrow_back</span> 
+            </Link>
+            </div>
 
-            <div className="mb-4 text-sm text-orange-200 w-3/4 mx-auto">
+            <div className="mb-4 text-sm text-orange-300 w-3/4 mx-auto text-center">
                 Forgot your password? No problem. Just let us know your email address and we will email you a password
                 reset link that will allow you to choose a new one.
             </div>
@@ -32,7 +37,7 @@ export default function ForgotPassword({ status }) {
                     type="email"
                     name="email"
                     value={data.email}
-                    className="mt-1 w-3/4"
+                    className="mt-1 w-full"
                     isFocused={true}
                     onChange={(e) => setData('email', e.target.value)}
                 />
